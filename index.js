f600b7b76f1ec859bcc987a3aba762e2c71c7c73
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-09-23 09:17:36 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Declare customerName in the global scope using var
+var customerName = "bob";
+
+// You can access customerName from anywhere in your code
+console.log(customerName); // Output: "bob"
+
+customerName = "bob";
+
+function upperCaseCustomerName() {
+    if(typeof customerName === "string") {
+        customerName = customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+}
+
+const leastFavoriteCustomer = "Peter"; 
+
+function changeLeastFavoriteCustomer(newCustomer) {
+    leastFavoriteCustomer = newCustomer;
+}
